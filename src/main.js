663ae97e5ebb79330fe33dbd78f7f8cefec9e592
shuirong <shuirong1997@icloud.com>
2017-05-23 09:44:24 +0800

    修改掉Icon的导入后缀名．因为这可能会导致报错

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Icon from 'vue-svg-icon/Icon.vue';
+import Icon from 'vue-svg-icon/Icon';
 import ElementUI from 'element-ui';
 import VueResource from 'vue-resource';
 import App from './App';
